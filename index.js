7618e28af6b175ebd4ad00534f5cae061e5e84f7
Alex Ortega <alexortegazj@gmail.com>
2021-09-26 17:51:49 -0700

    Updating websocket connection

--- index.js
+++ index.js
@@ -12,25 +12,32 @@ const webSocketServer = new WebSocket.Server({ server });
 const webSockets = {};
 
 webSocketServer.on('connection', function (webSocket, req) {
-    var userID = req.url.substr(1)
-    webSockets[userID] = webSocket
-    console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(webSockets))
+    var userID = req.url.substr(1);
+    if (userID) {
+      webSockets[userID] = webSocket
+      console.log(`Use Connected: ${userID}. At: ${new Date().toISOString()}`);
+    } else {
+      console.log('Anonymus user connected');
+    }
 
     webSocket.on('message', function(message) {
-      console.log('received from ' + userID + ': ' + message)
-      var messageArray = JSON.parse(message)
-      var toUserWebSocket = webSockets[messageArray[0]]
-      if (toUserWebSocket) {
-        console.log('sent to ' + messageArray[0] + ': ' + JSON.stringify(messageArray))
-        messageArray[0] = userID
-        toUserWebSocket.send(JSON.stringify(messageArray))
+      console.log('received from ' + userID + ': ' + message);
+      var messageBody = JSON.parse(message);
+      if(messageBody.length > 0) {
+        const [toUserId, message] = messageBody;
+        console.log(toUserId, message);
+        const toUserWebSocket = webSockets[toUserId]
+        if (toUserWebSocket) {
+          console.log('message sended to: ' + toUserId);
+          toUserWebSocket.send(JSON.stringify(message))
+        }
       }
-    })
+    });
   
     webSocket.on('close', function () {
       delete webSockets[userID]
       console.log('deleted: ' + userID)
-    })
+    });
   })
 
 
